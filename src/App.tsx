8cbb520baa8ed594e8192cf283fa08b691588b1a
LENOVO-EET\LENOVO_EET <pawel.mirys@gmail.com>
2023-03-02 13:08:08 +0100

    unfinished button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { MainMenuGlobalComponent } from '@/global-components/MainMenuGlobalComponent/MainMenuGlobalComponent';
+import { Button } from './global-components/Button/Button';
 
 const App = () => (
   <>
